b2064cb19aa1a04e61bf0a9477c9a152a5c29f56
hkuhn42 <harald.kuhn@gmail.com>
2016-02-21 16:03:47 +0100

    Initial version of audio api moved from sylvani

---
+++ bundles/io/org.eclipse.smarthome.io.audio/src/main/java/org/eclipse/smarthome/io/audio/AudioException.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2015-2016 Harald Kuhn
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.io.audio;
+
+/**
+ * General purpose audio exception
+ *
+ * @author Harald Kuhn (hkuhn42) initial api
+ */
+public class AudioException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public AudioException() {
+        super();
+    }
+
+    public AudioException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AudioException(String message) {
+        super(message);
+    }
+
+    public AudioException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ bundles/io/org.eclipse.smarthome.io.audio/src/main/java/org/eclipse/smarthome/io/audio/UnsupportedAudioFormatException.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2015-2016 Harald Kuhn
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.io.audio;
+
+/**
+ * Thrown when a requested format is not supported by an {@link AudioSource} or {@link AudioOutput} implementation
+ * 
+ * @author hkuhn
+ */
+public class UnsupportedAudioFormatException extends AudioException {
+
+    private static final long serialVersionUID = 1L;
+
+    private AudioFormat unsupportedFormat;
+
+    public UnsupportedAudioFormatException(String message, AudioFormat unsupportedFormat, Throwable cause) {
+        super(message, cause);
+        this.unsupportedFormat = unsupportedFormat;
+    }
+
+    public UnsupportedAudioFormatException(String message, AudioFormat unsupportedFormat) {
+        this(message, unsupportedFormat, null);
+    }
+
+    public AudioFormat getUnsupportedFormat() {
+        return unsupportedFormat;
+    }
+}
