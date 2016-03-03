d40e5765fe2838d45d2cdfaefdd922a36652edbb
Kelly Davis <kdavis@mozilla.com>
2016-03-03 18:10:50 +0100

    Updated API
    Signed-off-by: Kelly Davis <kdavis@mozilla.com>

--- bundles/io/org.eclipse.smarthome.io.audio/src/main/java/org/eclipse/smarthome/io/audio/UnsupportedAudioFormatException.java
+++ bundles/io/org.eclipse.smarthome.io.audio/src/main/java/org/eclipse/smarthome/io/audio/UnsupportedAudioFormatException.java
@@ -9,25 +9,47 @@
 package org.eclipse.smarthome.io.audio;
 
 /**
- * Thrown when a requested format is not supported by an {@link AudioSource} or {@link AudioSink} implementation
+ * Thrown when a requested format is not supported by an {@link AudioSource}
+ * or {@link AudioSink} implementation
  * 
- * @author hkuhn
+ * @author Harald Kuhn (hkuhn42) initial api
  */
 public class UnsupportedAudioFormatException extends AudioException {
 
     private static final long serialVersionUID = 1L;
 
+   /**
+    * Unsupported {@link AudioFormat}
+    */
     private AudioFormat unsupportedFormat;
 
+   /**
+    * Constructs a new exception with the specified detail message, unsupported format, and cause.
+    *
+    * @param message Detail message
+    * @param unsupportedFormat Unsupported format
+    * @param cause The cause 
+    */
     public UnsupportedAudioFormatException(String message, AudioFormat unsupportedFormat, Throwable cause) {
         super(message, cause);
         this.unsupportedFormat = unsupportedFormat;
     }
 
+   /**
+    * Constructs a new exception with the specified detail message and unsupported format.
+    *
+    * @param message Detail message
+    * @param unsupportedFormat Unsupported format
+    */
     public UnsupportedAudioFormatException(String message, AudioFormat unsupportedFormat) {
         this(message, unsupportedFormat, null);
     }
 
+   /**
+    * Gets the unsupported format
+    *
+    * @return The unsupported format
+    */
     public AudioFormat getUnsupportedFormat() {
         return unsupportedFormat;
     }
